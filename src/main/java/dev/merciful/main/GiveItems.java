5a4a44329f3ab8d7e5baa20c6af9737fbd056984
DumbDergMerciful <94778270+DumbDergMerciful@users.noreply.github.com>
2022-08-22 05:01:22 -0500

    Update GiveItems.java

--- src/main/java/dev/merciful/main/GiveItems.java
+++ src/main/java/dev/merciful/main/GiveItems.java
@@ -7,7 +7,6 @@ public class GiveItems extends BukkitRunnable {
 
     @Override
     public void run() {
-        // What you want to schedule goes here
      if(Plugin.plugin.getConfig().getBoolean("Enabled")){
          RandomItem.givePlayersItem();
      }else {
